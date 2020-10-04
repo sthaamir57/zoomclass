377bccef104755784a7d5c623c1e9a4aa37ac6bd
Amir <stha.amir57@gmail.com>
2020-10-04 12:25:47 +0545

    oop pass alert disable

--- scripts/app.js
+++ scripts/app.js
@@ -30,9 +30,9 @@ if (day === 6) {
 } else if (day === 0 || day === 1 || day === 2) {
   // firstSubjects.classList.remove("opacity-0");
   subFirst(day);
-  if (timeNow === 13 || timeNow === 14) {
-    alert("OOP password is in Sir's fb group.");
-  }
+  // if (timeNow === 13 || timeNow === 14) {
+  //   alert("OOP password is in Sir's fb group.");
+  // }
 } else {
   // lastSubjects.classList.remove("opacity-0");
   subLast(day);
